679223deea3aefec5db25a5a35bfe0fa22677df3
narra <ermanfaminiano020@gmail.com>
2025-04-04 08:57:23 +0800

    feat: :sparkles: added data

---
+++ data/features.js
@@ -0,0 +1,29 @@
+import { CloudArrowUpIcon, LockClosedIcon, ArrowPathIcon } from '@heroicons/react/24/outline';
+
+
+const features = [
+    {
+      name: 'Push to deploy',
+      description:
+        'Commodo nec sagittis tortor mauris sed. Turpis tortor quis scelerisque diam id accumsan nullam tempus. Pulvinar etiam lacus volutpat eu. Phasellus praesent ligula sit faucibus.',
+      href: '#',
+      icon: CloudArrowUpIcon,
+    },
+    {
+      name: 'SSL certificates',
+      description:
+        'Pellentesque enim a commodo malesuada turpis eleifend risus. Facilisis donec placerat sapien consequat tempor fermentum nibh.',
+      href: '#',
+      icon: LockClosedIcon,
+    },
+    {
+      name: 'Simple queues',
+      description:
+        'Pellentesque sit elit congue ante nec amet. Dolor aenean curabitur viverra suspendisse iaculis eget. Nec mollis placerat ultricies euismod ut condimentum.',
+      href: '#',
+      icon: ArrowPathIcon,
+    },
+  ];
+
+
+  export default features;
---
+++ data/services.js
@@ -0,0 +1,19 @@
+const services = [
+    {
+      icon: 'ph-desktop',
+      title: 'Web Development',
+      description: 'Custom websites built with the latest technologies to ensure performance and scalability.'
+    },
+    {
+      icon: 'ph-android-logo',
+      title: 'Mobile App Development',
+      description: 'High-quality mobile applications tailored to your business needs.'
+    },
+    {
+      icon: 'ph-code',
+      title: 'Software Solutions',
+      description: 'Robust and scalable software solutions to enhance business operations.'
+    }
+  ];
+
+export default services;
