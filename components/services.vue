63ef491dd87f87dd8990948e5ce38fbfe51f2341
narra <ermanfaminiano020@gmail.com>
2025-04-04 08:57:43 +0800

    feat: :sparkles: turned section into a components

---
+++ components/services.vue
@@ -0,0 +1,42 @@
+<template>
+    <section class="bg-slate-950 py-24 border-y-2 border-primary sm:py-32">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:text-center" v-motion :initial="{ opacity: 0, y: 30 }"
+                :visible="{ opacity: 1, y: 0, transition: { duration: 800 } }">
+                <p
+                    class="mt-2 text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl lg:text-balance">
+                    Our Services</p>
+                <p class="mt-6 text-lg/8 text-gray-400">We provide a range of digital solutions to help businesses grow
+                    and
+                    succeed.</p>
+            </div>
+            <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+                <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+                    <div v-for="(service, index) in services" :key="index" class="flex flex-col" v-motion
+                        :initial="{ opacity: 0, y: 50 }" :visible="{
+                            opacity: 1,
+                            y: 0,
+                            transition: {
+                                delay: index * 200,
+                                duration: 800
+                            }
+                        }">
+                        <dt class="flex items-center gap-x-3 text-base/7 font-semibold text-white">
+                            <Icon :name="service.icon" class="size-5 flex-none text-primary" aria-hidden="true" />
+                            {{ service.title }}
+                        </dt>
+                        <dd class="mt-4 flex flex-auto flex-col text-base/7 text-gray-400">
+                            <p class="flex-auto">
+                                {{ service.description }}
+                            </p>
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+import services from '~/data/services';
+</script>
---
+++ components/team.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="bg-gray-900 py-24 sm:py-32">
+        <div class="mx-auto grid max-w-7xl gap-20 px-6 lg:px-8 xl:grid-cols-3">
+            <div class="max-w-xl" v-motion :initial="{ opacity: 0, y: 30 }"
+                :visible="{ opacity: 1, y: 0, transition: { duration: 800 } }">
+                <h2 class="text-3xl font-semibold tracking-tight text-pretty text-white sm:text-4xl">Meet our team</h2>
+                <p class="mt-6 text-lg/8 text-gray-300">We're a dynamic group of individuals who are passionate about
+                    what
+                    we
+                    do and dedicated to delivering the best results for our clients.</p>
+            </div>
+            <ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+                <li v-for="(member, index) in teamMembers" :key="index" v-motion :initial="{ opacity: 0, x: 30 }"
+                    :visible="{
+                        opacity: 1,
+                        x: 0,
+                        transition: {
+                            delay: index * 300,
+                            duration: 800,
+                            ease: 'easeOut'
+                        }
+                    }">
+                    <div class="flex items-center gap-x-6">
+                        <img class="size-16 rounded-full" :src="member.image" :alt="member.name">
+                        <div>
+                            <h3 class="text-base/7 font-semibold tracking-tight text-white">{{ member.name }}</h3>
+                            <p class="text-sm/6 font-semibold text-primary">{{ member.position }}</p>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+import teamMembers from '~/data/team';
+</script>
