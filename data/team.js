63b7d7dbfc9b91d3c5276e062789137dae8f34e7
ErmanDev <ermanfaminiano020@gmail.com>
2026-07-20 07:58:21 +0800

    new pics and escalation of member position.

--- data/team.js
+++ data/team.js
@@ -1,14 +1,14 @@
 const teamMembers = [
   {
     name: "Jonhyl Balsamo",
-    position: "Founder/Frontend Web Developer",
+    position: "Founder/Full Stack Developer",
     image: "/images/team/founder_1.jpg",
     description:
       "Jonhyl has hands-on experience working with international companies, collaborating with teams across the UK, Europe, and Asia. His expertise includes building high-quality, scalable frontend architectures and delivering modern, user-focused web experiences.",
   },
   {
     name: "Erman Faminiano",
-    position: "Co Founder/Full Stack Web/Mobile Developer",
+    position: "Co Founder/Full Stack Web/Mobile Developer/Game Developer",
     image: "/images/team/founder_2.jpg",
     description:
       "Erman has extensive freelance experience, delivering a wide range of full-stack and mobile development projects for clients across different industries. He also leads tech workshops, sharing his knowledge in modern development practices and helping aspiring developers strengthen their skills.",
